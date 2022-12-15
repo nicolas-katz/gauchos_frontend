d854f6418a4c021a87eecbf7a2e3c3c55856a2ed
nicolas-katz <83619866+nicolas-katz@users.noreply.github.com>
2022-12-14 21:22:37 -0300

    adding @import styled-components to files

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,36 @@ import About from './views/About';
 import Contact from './views/Contact';
 import Login from './views/Login';
 import Admin from './views/Admin';
+import NotFound from './views/NotFound';
 import { ProtectedLogin, ProtectedAdmin } from './components/ProtectedRoutes';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
     return (
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/coleccion' element={<Colection />} />
-        <Route path='/coleccion/:category' element={<Colection />} />
-        <Route path='/coleccion/:id' element={<Product />} />
-        <Route path='/galeria' element={<Gallery />} />
-        <Route path='/empresa' element={<About />} />
-        <Route path='/contacto' element={<Contact />} />
-        <Route path='/acceso/login' element={
-          <ProtectedLogin>
-            <Login />
-          </ProtectedLogin>
-        } />
-        <Route path='/acceso/administrador' element={
-          <ProtectedAdmin>
-            <Admin />
-          </ProtectedAdmin>
-        } />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
+        <div>
+          <Header />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/coleccion' element={<Colection />} />
+            <Route path='/coleccion/:category' element={<Colection />} />
+            <Route path='/coleccion/:id' element={<Product />} />
+            <Route path='/galeria' element={<Gallery />} />
+            <Route path='/empresa' element={<About />} />
+            <Route path='/contacto' element={<Contact />} />
+            <Route path='/acceso/login' element={
+              <ProtectedLogin>
+                <Login />
+              </ProtectedLogin>
+            } />
+            <Route path='/acceso/administrador' element={
+              <ProtectedAdmin>
+                <Admin />
+              </ProtectedAdmin>
+            } />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
     );
 };
